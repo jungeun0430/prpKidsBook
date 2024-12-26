8db044c108da7747bba9d3adbbd674177f8df2c1
jungeun0430 <je.lee@iea.co.kr>
2024-12-26 10:28:48 +0900

    프로키즈북  - 메타태그 수정

--- resources/js/main.js
+++ resources/js/main.js
@@ -54,23 +54,23 @@ document.addEventListener("DOMContentLoaded", function(){
             <div class="footer-info-wrap">
                 <div>
                     <ul class="policy-wrap">
-                        <li><a href="/pages/policy/terms-of-policy">이용약관</a></li>
-                        <li><a href="/pages/policy/privacy-policy">개인정보처리방침</a></li>
+                        <li><a href="/pages/policy/terms-of-policy" data-translate="footer_1">이용약관</a></li>
+                        <li><a href="/pages/policy/privacy-policy" data-translate="footer_2">개인정보처리방침</a></li>
                     </ul>
                     <ul class="place-info">
-                        <li>주소 : 서울특별시 송파구 법원로8길 8 SKV1 910호 (05855)</li>
-                        <li>대표전화 : 02-6269-0630</li>
-                        <li class="copyright">Copyright ⓒ IEA. All Rights Reserved.</li>
+                        <li data-translate="footer_3">주소 : 서울특별시 송파구 법원로8길 8 SKV1 910호 (05855)</li>
+                        <li data-translate="footer_4">대표전화 : 02-6269-0630</li>
+                        <li class="copyright" data-translate="footer_5">Copyright ⓒ IEA. All Rights Reserved.</li>
                     </ul>
                 </div>
                 <div class="select-box dropdown2">
                     <button class="label" data-value="Family Site">
-                        <span>패밀리 사이트</span>
+                        <span data-translate="footer_6">패밀리 사이트</span>
                     </button>
                     <ul class="option-list">
-                        <li><a href="#">프로</a></li>
-                        <li><a href="#">IEA</a></li>
-                        <li><a href="#">제이비트리</a></li>
+                        <li><a href="#" data-translate="footer_7">프로</a></li>
+                        <li><a href="#" >IEA</a></li>
+                        <li><a href="#" data-translate="footer_8">제이비트리</a></li>
                     </ul>
                 </div>
             </div>
@@ -86,15 +86,10 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   /* 초기 설정(KOR) : localstorage에 저장된 값이 없을 시 */
     rendererHeader(selectedLanguageOption)
-    translateText(selectedLanguageOption, '/index')
+    translateText(selectedLanguageOption, path)
 
   const navItems = document.querySelectorAll('nav ul li a');
   const logoLink = document.querySelector('h1 a');
-
-  /*if (!activePath) {
-    activePath = '/index';
-    localStorage.setItem('activeNavPath', activePath);
-  }*/
 
   // 페이지 로드 시 저장된 경로를 기준으로 'on' 클래스 적용
   navItems.forEach((item) => {
@@ -208,7 +203,7 @@ document.addEventListener("DOMContentLoaded", function(){
       // 초기 옵션 렌더링
       renderOptions(localStorage.getItem('selectedOption') || options[0]);
     }
-    function translateText (option,path='/index') {
+    function translateText (option,path) {
       // 번역
       fetch(`/resources/translation-data/${option}.json`)
         .then(response => {
@@ -245,7 +240,7 @@ document.addEventListener("DOMContentLoaded", function(){
   // 1-2. 텍스트만 포함된 옵션 렌더링 함수 (링크 이동)
     function renderOptionTextOnly(li, item) {
       const a = document.createElement('a');
-      a.textContent = item.option;
+      a.textContent = item.option[localStorage.getItem('selectedOption')];
       a.href = item.link;
       a.target = '_blank';
       a.addEventListener('click', (event) => {
@@ -257,32 +252,46 @@ document.addEventListener("DOMContentLoaded", function(){
   // 언어 번역
     function updateLanguage(language, path) {
       switch (path) {
-        case "/index":
-          indexPageTranslate(language);
-          break;
+      case "/index":
+        indexPageTranslate(language);
+        break;
       case "/":
         indexPageTranslate(language);
         break;
-        case "/pages/contact":
-          contactPageTranslate(language);
-          break;
+      case "/pages/contact":
+        contactPageTranslate(language);
+        break;
         case "/pages/policy/privacy-policy":
           break;
         default:
           break;
       }
+      // 드롭다운의 텍스트도 업데이트
+      const dropdown2 = document.querySelector('.dropdown2');
+      const options = dropdown2.querySelectorAll('.option-item');
+
+      options.forEach(option => {
+        const link = option.querySelector('a');
+        const key = link.textContent; // 예를 들어, '프로', 'IEA', '제이비트리'
+        const translated = language[key]; // 언어 JSON으로부터 번역된 텍스트 가져오기
+
+        if (translated) {
+          link.textContent = translated; // 텍스트 업데이트
+          localStorage.setItem('selectedOption', translated);
+        }
+      });
     }
   // 1-3. 각 드롭다운에 맞게 초기화
     // 실행문 : header dropdown
     const dropdown1 = document.querySelector('.dropdown1');
-    initDropdown(dropdown1, ['KOR'], renderOptionWithImage); //['KOR', 'ENG', 'JPN']
+    initDropdown(dropdown1, ['KOR', 'ENG', 'JPN'], renderOptionWithImage); //['KOR', 'ENG', 'JPN']
 
     // 실행문 : footer dropdown
     const dropdown2 = document.querySelector('.dropdown2');
     initDropdown(dropdown2, [
-      {option: '프로', link: 'https://procorp.co.kr/'},
-      {option: 'IEA', link: 'https://www.iea.co.kr/'},
-      {option: '제이비트리', link: 'https://jbtree.co.kr/'}
+      {option: {'KOR':'프로','ENG':'Pro','JPN':'Pro'}, link: 'https://procorp.co.kr/'},
+      {option: {'KOR':'IEA','ENG':'IEA','JPN':'IEA'}, link: 'https://www.iea.co.kr/'},
+      {option: {'KOR':'제이비트리','ENG':'JBTree','JPN':'JBTree'}, link: 'https://jbtree.co.kr/'}
     ], renderOptionTextOnly);
 });
 
@@ -296,6 +305,14 @@ function indexPageTranslate(language) {
   document.querySelector('[data-translate="sec1_apple"]').alt = language.sec1_apple.alt;
   document.querySelector('[data-translate="sec1_google"]').src = language.sec1_google.src;
   document.querySelector('[data-translate="sec1_google"]').alt = language.sec1_google.alt;
+  let elementSec1 = document.querySelector('[data-translate="sec1_class"]');
+  let elementSec1ClassNames = [...elementSec1.classList];
+  elementSec1ClassNames.forEach(className => {
+    if(className !== "inner-wrap") {
+      elementSec1.classList.remove(className)
+    }
+  })
+  document.querySelector('[data-translate="sec1_class"]').classList.add(language.sec1_class);
   /* sec2 번역 */
   document.querySelector('[data-translate="sec2_title"]').innerHTML = language.sec2_title;
   document.querySelector('[data-translate="sec2_desc"]').innerHTML = language.sec2_desc;
@@ -322,6 +339,9 @@ function indexPageTranslate(language) {
   document.querySelector('[data-translate="sec5_desc"]').innerHTML = language.sec5_desc;
   document.querySelector('[data-translate="sec5_phone1"]').src = language.sec5_phone1;
   document.querySelector('[data-translate="sec5_phone2"]').src = language.sec5_phone2;
+  document.querySelector('[data-translate="sec5_style"]').style.height = language.sec5_style
+  document.querySelector('[data-translate="sec4_style"]').style.height = language.sec4_style
+  // document.querySelector('[data-translate="sec4_style"]').style.height = language.sec5_style
   /* sec6 */
   document.querySelector('[data-translate="sec6_title"]').innerHTML = language.sec6_title;
   document.querySelector('[data-translate="sec6_desc"]').innerHTML = language.sec6_desc;
@@ -335,8 +355,41 @@ function indexPageTranslate(language) {
   /* sec8 */
   document.querySelector('[data-translate="sec8_star"]').style = language.sec8_star;
   document.querySelector('[data-translate="sec8_btn"]').innerHTML = language.sec8_btn;
+  document.querySelector('[data-translate="footer_1"]').innerText = language.footer_1;
+  document.querySelector('[data-translate="footer_2"]').innerText = language.footer_2;
+  document.querySelector('[data-translate="footer_3"]').innerText = language.footer_3;
+  document.querySelector('[data-translate="footer_4"]').innerText = language.footer_4;
+  document.querySelector('[data-translate="footer_5"]').innerText = language.footer_5;
+  document.querySelector('[data-translate="footer_6"]').innerText = language.footer_6;
+  /*document.querySelector('[data-translate="footer_7"]').innerText = language.footer_7;
+  document.querySelector('[data-translate="footer_8"]').innerText = language.footer_8;*/
 }
 /* Contact 페이지 */
 function contactPageTranslate (language) {
-  document.querySelector('[data-translate="sec1_text2"]').innerText = language.sec1_text2;
+  document.querySelector('[data-translate="contact_1"]').innerHTML = language.contact_1;
+  document.querySelector('[data-translate="contact_2"]').innerText = language.contact_2;
+  document.querySelector('[data-translate="contact_3"]').innerText = language.contact_3;
+  document.querySelector('[data-translate="contact_4"]').placeholder = language.contact_4;
+  document.querySelector('[data-translate="contact_5"]').innerText = language.contact_5;
+  document.querySelector('[data-translate="contact_6"]').placeholder = language.contact_6;
+  document.querySelector('[data-translate="contact_7"]').innerText = language.contact_7;
+  document.querySelector('[data-translate="contact_8"]').placeholder = language.contact_8;
+  document.querySelector('[data-translate="contact_9"]').innerText = language.contact_9;
+  document.querySelector('[data-translate="contact_10"]').placeholder = language.contact_10;
+  document.querySelector('[data-translate="contact_11"]').innerHTML = language.contact_11;
+  document.querySelector('[data-translate="contact_12"]').placeholder = language.contact_12;
+  document.querySelector('[data-translate="contact_13"]').innerHTML = language.contact_13;
+  document.querySelector('[data-translate="contact_14"]').placeholder = language.contact_14;
+  document.querySelector('[data-translate="contact_15"]').innerHTML = language.contact_15;
+  document.querySelector('[data-translate="contact_16"]').placeholder = language.contact_16;
+  document.querySelector('[data-translate="contact_17"]').innerText = language.contact_17;
+  document.querySelector('[data-translate="contact_18"]').innerHTML = language.contact_18;
+  document.querySelector('[data-translate="footer_1"]').innerText = language.footer_1;
+  document.querySelector('[data-translate="footer_2"]').innerText = language.footer_2;
+  document.querySelector('[data-translate="footer_3"]').innerText = language.footer_3;
+  document.querySelector('[data-translate="footer_4"]').innerText = language.footer_4;
+  document.querySelector('[data-translate="footer_5"]').innerText = language.footer_5;
+  document.querySelector('[data-translate="footer_6"]').innerText = language.footer_6;
+  /*document.querySelector('[data-translate="footer_7"]').innerText = language.footer_7;
+  document.querySelector('[data-translate="footer_8"]').innerText = language.footer_8;*/
 }
